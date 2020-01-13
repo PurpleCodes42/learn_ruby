7405adbfde221409ba056d8a59eb7c010a58ee1f
Purple <purplecodes42@gmail.com>
2020-01-13 19:57:12 +0100

    Timer ready

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -2,8 +2,18 @@ class Timer
   def initialize
     @seconds = 0
   end
-
+ 
+  # die beiden folgen methoden können auch als attr:accessor :seconds gecoded werden
   def seconds 
     @seconds
   end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+  Time.at(@seconds).utc.strftime("%H:%M:%S")
+  end
 end
+
